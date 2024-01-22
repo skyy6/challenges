72ea987c1d0ba08104113499592374b56590ddc1
skyy6 <71879645+skyy6@users.noreply.github.com>
2024-01-23 00:55:06 +0100

    Updated old Bluetooth file

--- KattisProblem/Bluetooth.cs
+++ KattisProblem/Bluetooth.cs
@@ -4,19 +4,15 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-
 internal class Program
 {
     private static void Main(string[] args)
     {
         List<string> list = new List<string>();
-
         List<string> leftSideList = new List<string>();
-
         List<string> rightSideList = new List<string>();
 
         bool blueOnLeftSide = false;
-
         bool blueOnRightSide = false;
 
         int value;
@@ -30,7 +26,6 @@ internal class Program
         foreach (string str in list)
         {
             bool blueFound = false;
-
             if (str.Contains('b'))
             {
                 blueFound = true;
@@ -54,34 +49,17 @@ internal class Program
             }
 
         }
-        int test = leftSideList.Where(x => x != null && x.Contains('-')).Count();
-
-        if (!blueOnLeftSide && !isTeethMissing(leftSideList))
-        {
-            value = 0;
-        }
-        else if (!blueOnRightSide && !isTeethMissing(rightSideList))
-        {
-            value = 1;
-        }
-        else
-        {
-            value = 2;
-        }
-
+		
+		value = (!blueOnLeftSide && !isTeethMissing(leftSideList)) ? 0 :
+		(!blueOnRightSide && !isTeethMissing(rightSideList)) ? 1 : 2;
+				
         Console.WriteLine(value);
-
     }
-
     static bool isTeethMissing(List<string> teethlist)
     {
         int lowerJawValue = teethlist.Where(x => x != null && x.Contains('-')).Count();
         int upperJawValue = teethlist.Where(x => x != null && x.Contains('+')).Count();
            
-        return upperJawValue == 8 || lowerJawValue == 8;
-        
+        return upperJawValue == 8 || lowerJawValue == 8;   
     }
-
-
-
 }
