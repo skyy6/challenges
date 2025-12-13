8f566a04b4e6aeb1e562e8bc9ad341590786a73e
skyy6 <freddee97@gmail.com>
2025-12-13 15:51:29 +0100

    Added code for day4 part 2

--- AdventOfCode/2025/Day4.cs
+++ AdventOfCode/2025/Day4.cs
@@ -10,13 +10,14 @@ namespace AoC25
 {
     public class Day4
     {
-        List<string> list = Helper.GetListFromFileDay4(
+        List<char[]> list = Helper.GetCharListFromFileDay4(
             @"C:\Users\skyy6\Desktop\AoC25files\aoc254.txt"
         );
 
         public int AoC4part1()
         {
-            string rowToInsert = new string('.', list[0].Length);
+            char[] rowToInsert = new char[list[0].Length];
+            Array.Fill(rowToInsert, '.');
             int numOfRolls = 0;
 
             list.Insert(0, rowToInsert);
@@ -40,6 +41,40 @@ namespace AoC25
             return numOfRolls;
         }
 
+        public int AoC4part2()
+        {
+            char[] rowToInsert = new char[list[0].Length];
+            Array.Fill(rowToInsert, '.');
+            int numOfRolls = 0;
+            bool foundRolls;
+
+            list.Insert(0, rowToInsert);
+            list.Add(rowToInsert);
+
+            do
+            {
+                foundRolls = false;
+                for (int i = 1; i < list.Count - 1; i++)
+                {
+                    for (int j = 0; j < list[i].Length; j++)
+                    {
+                        if (list[i][j] == '.')
+                        {
+                            continue;
+                        }
+                        if (CanAccessRoll(i, j))
+                        {
+                            numOfRolls++;
+                            list[i][j] = '.';
+                            foundRolls = true;
+                        }
+                    }
+                }
+            } while (foundRolls);
+
+            return numOfRolls;
+        }
+
         private bool CanAccessRoll(int listIndex, int stringIndex)
         {
             char[] adjChars =
@@ -53,7 +88,7 @@ namespace AoC25
                 list[listIndex - 1][stringIndex - 1],
                 list[listIndex - 1][stringIndex],
             ];
-            int ocurr = adjChars.Select(x => x == '@').Count();
+            int ocurr = adjChars.Count(x => x == '@');
 
             return ocurr < 4;
         }
